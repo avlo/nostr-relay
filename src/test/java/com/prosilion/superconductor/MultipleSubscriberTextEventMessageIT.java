ab0828825321778181d706ae4b961735a324ef19
nick avlo <avlo@yahoo.com>
2024-06-28 20:39:53 -0700

    paramd json start

--- src/test/java/com/prosilion/superconductor/MultipleSubscriberTextEventMessageIT.java
+++ src/test/java/com/prosilion/superconductor/MultipleSubscriberTextEventMessageIT.java
@@ -1,13 +1,13 @@
 package com.prosilion.superconductor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.util.concurrent.MoreExecutors;
 import lombok.Getter;
 import lombok.Synchronized;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -28,10 +28,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -39,75 +37,82 @@ import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext
 @ContextConfiguration
 @TestPropertySource("/application-test.properties")
 class MultipleSubscriberTextEventMessageIT {
-  private static final String TARGET_TEXT_MESSAGE_EVENT_CONTENT = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
+  private static String websocketUrl;
 
-  public final String textMessageEventJson;
-  public final String textMessageEventJsonReordered;
-
-  private static final String SCHEME_WS = "ws";
-  private static final String HOST = "localhost";
-  private final String websocketUrl;
-
-  private final String hexCounterSeed;
-  private final int hexStartNumber;
-  private final Integer targetCount;
+  private static String hexCounterSeed;
+  private static int hexStartNumber;
+  private static Integer targetCount;
   private final Integer pctThreshold;
   int resultCount;
-
-  private final ObjectMapper mapper = new ObjectMapper();
-  private final ExecutorService executorService;
-
-  List<Callable<CompletableFuture<WebSocketSession>>> reqClients;
+  private final ObjectMapper mapper;
+  private static ExecutorService executorService;
+  String inJson;
+  String expectedJson;
 
   MultipleSubscriberTextEventMessageIT(
       @Value("${server.port}") String port,
       @Value("${superconductor.test.req.hexCounterSeed}") String hexCounterSeed,
       @Value("${superconductor.test.req.instances}") Integer reqInstances,
       @Value("${superconductor.test.req.success_threshold_pct}") Integer pctThreshold) throws IOException {
-    this.websocketUrl = SCHEME_WS + "://" + HOST + ":" + port;
+    this.websocketUrl = "ws://localhost:5555";
     this.hexCounterSeed = hexCounterSeed;
     this.hexStartNumber = Integer.parseInt(hexCounterSeed, 16);
     this.targetCount = reqInstances;
     this.pctThreshold = pctThreshold;
-    this.executorService = MoreExecutors.newDirectExecutorService();
 
-    this.textMessageEventJson = Files.lines(Paths.get("src/test/resources/text_event_json_input.txt")).collect(Collectors.joining("\n"));
-    this.textMessageEventJsonReordered = Files.lines(Paths.get("src/test/resources/text_event_json_reordered.txt")).collect(Collectors.joining("\n"));
+    this.executorService = Executors.newFixedThreadPool(targetCount + 1);
+    this.mapper = new ObjectMapper();
+    this.resultCount = 0;
+    this.inJson = Files.lines(Paths.get("src/test/resources/text_event_json_input.txt")).collect(Collectors.joining("\n"));
+    this.expectedJson = Files.lines(Paths.get("src/test/resources/text_event_json_reordered.txt")).collect(Collectors.joining("\n"));
+    this.stuff = new ArrayList<>();
+
+    stuff.add(Map.of(this.inJson, this.expectedJson));
   }
+
+  static List<Map<String, String>> stuff;
 
   @BeforeAll
   public void setup() {
-    WebSocketStompClient eventStompClient = new WebSocketStompClient(new StandardWebSocketClient());
-    eventStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-    CompletableFuture<WebSocketSession> eventExecute = eventStompClient.getWebSocketClient().execute(new EventMessageSocketHandler(), websocketUrl, "");
-    await().until(eventExecute::isDone);
+    stuff.forEach(map -> map.forEach((key, value) -> {
+      WebSocketStompClient eventStompClient = new WebSocketStompClient(new StandardWebSocketClient());
+      eventStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+      CompletableFuture<WebSocketSession> eventExecute = eventStompClient.getWebSocketClient().execute(new EventMessageSocketHandler(key), websocketUrl, "");
+      await().until(eventExecute::isDone);
 
-    reqClients = new ArrayList<>(targetCount);
-    IntStream.range(0, targetCount).parallel().forEach(increment -> {
-      final WebSocketStompClient reqStompClient = new WebSocketStompClient(new StandardWebSocketClient());
-      reqStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+      await().until(() -> eventExecute.get().isOpen());
 
-      Callable<CompletableFuture<WebSocketSession>> callableTask = () -> {
-        return reqStompClient.getWebSocketClient().execute(new ReqMessageSocketHandler(increment, getNextHex(increment)), websocketUrl, "");
-      };
-      reqClients.add(callableTask);
-    });
-//    System.out.println("reqClients count (1): " + reqClients.size());
-    assertDoesNotThrow(() -> executorService.invokeAll(reqClients).stream().parallel().forEach(future ->
-        await().until(() -> future.get().isDone())));
+      List<Callable<CompletableFuture<WebSocketSession>>> reqClients = new ArrayList<>(targetCount);
+      IntStream.range(0, targetCount).parallel().forEach(increment -> {
+        WebSocketStompClient reqStompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        reqStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        Callable<CompletableFuture<WebSocketSession>> callableTask = () ->
+            reqStompClient.getWebSocketClient().execute(
+                new ReqMessageSocketHandler(
+                    increment,
+                    getNextHex(increment),
+                    value),
+                websocketUrl,
+                "");
+        reqClients.add(callableTask);
+      });
+
+      assertDoesNotThrow(() -> executorService.invokeAll(reqClients).stream().parallel().forEach(future ->
+          await().until(() -> future.get().isDone())));
+    }));
   }
 
   @Test
-  void testEventMessageThenReqMessage() {
+  void test1() {
     executorService.shutdown();
-    await().until(() -> executorService.awaitTermination(5000, TimeUnit.SECONDS));
-    await().until(executorService::isTerminated);
+    await().atMost(5, TimeUnit.SECONDS).until(executorService::isTerminated);
 
     System.out.println("-------------------");
     System.out.printf("[%s/%s] == [%d%% of minimal %d%%] completed before test-container thread ended%n",
@@ -118,16 +123,21 @@ class MultipleSubscriberTextEventMessageIT {
     System.out.println("-------------------");
   }
 
+  static class EventMessageSocketHandler extends TextWebSocketHandler {
+    private final String inJson;
 
-  class EventMessageSocketHandler extends TextWebSocketHandler {
+    public EventMessageSocketHandler(String inJson) {
+      this.inJson = inJson;
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-      session.sendMessage(new TextMessage(textMessageEventJson));
+      session.sendMessage(new TextMessage(inJson));
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-      assertTrue(message.getPayload().toString().contains(TARGET_TEXT_MESSAGE_EVENT_CONTENT));
+      assertTrue(message.getPayload().toString().contains("5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc"));
       session.close();
     }
   }
@@ -136,28 +146,26 @@ class MultipleSubscriberTextEventMessageIT {
   class ReqMessageSocketHandler extends TextWebSocketHandler {
     private final Integer index;
     private final String reqJson;
+    private final String expectedJson;
 
-    public ReqMessageSocketHandler(Integer index, String reqId) {
+    public ReqMessageSocketHandler(Integer index, String reqId, String expectedJson) {
       this.index = index;
+      this.expectedJson = expectedJson;
       reqJson = "[\"REQ\",\"" + reqId + "\",{\"ids\":[\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\"],\"authors\":[\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\"]}]";
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//      System.out.printf("AAAAAAAAAAAAAAAAAAAAAAAA[%02d], id: [%s]\n", index, session.getId());
       session.sendMessage(new TextMessage(reqJson));
     }
 
     @Override
     public void handleMessage(@NotNull WebSocketSession session, WebSocketMessage<?> message) throws EvaluationException, IOException {
-      boolean condition = ComparatorWithoutOrder.equalsJson(mapper.readTree(textMessageEventJsonReordered), mapper.readTree(message.getPayload().toString()));
+      boolean condition = ComparatorWithoutOrder.equalsJson(mapper.readTree(expectedJson), mapper.readTree(message.getPayload().toString()));
 
-//    below sout seems to serve extending thread execution time, preventing its premature shutdown
-      System.out.printf("BBBBBBBBBBBBBBBBBBBBBBBB[%02d], match: [%s]\n", index, condition);
       if (!condition) {
-//        System.out.println("CCCCCCCCCCCCCCCCCCCCCCCC");
         session.close();
-        throw new EvaluationException(String.format("Json doesnt' match.  Expected value:%n%s%n but received:%n%s%n", textMessageEventJsonReordered, mapper.readTree(message.getPayload().toString()).toPrettyString()));
+        throw new EvaluationException(String.format("Json doesnt' match.  Expected value:%n%s%n but received:%n%s%n", expectedJson, mapper.readTree(message.getPayload().toString()).toPrettyString()));
       }
       increment();
       session.close();
@@ -169,7 +177,7 @@ class MultipleSubscriberTextEventMessageIT {
     resultCount++;
   }
 
-  private String getNextHex(int i) {
+  private static String getNextHex(int i) {
     String incrementedHexNumber = Integer.toHexString(hexStartNumber + i);
     return hexCounterSeed
         .substring(0, hexCounterSeed.length() - incrementedHexNumber.length())
